b820b0a00894b430f1dd34cfdf033e0cc6d5bc3b
shifanafin <ctshifanachennara@gmail.com>
2022-07-09 16:22:18 +0530

    counting

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
 
 function App() {
+  var [count,setCount] = useState(0);
+
+function increase()
+{
+    setCount(count+1);
+
+}
+function decrease()
+{
+  setCount(count--)
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <div>
+      <button onClick={decrease}>-</button>
+      </div>
     </div>
   );
 }
